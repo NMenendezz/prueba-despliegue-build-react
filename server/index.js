2cf4b87fe6b6a75e5c8e6524cb971dfeb1c5f866
Nacho Menendez <nchmnndzlpz@gmail.com>
2024-04-03 13:53:48 +0200

    Eliminar el uso de cors en el servidor

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
