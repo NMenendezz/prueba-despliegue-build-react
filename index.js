8dcb29d6bdd8d03b16f530acbe9e222679dd033d
Nacho Menendez <nchmnndzlpz@gmail.com>
2024-04-03 20:19:13 +0200

    Actualizar rutas de archivos estáticos y comandos de construcción en package.json

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 
-// Hacer que node sirva los archivos de nuestro app React
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hola desde el servidor!" });
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
