92904beb4b9d64e3e6b2e1736c8449be3d2e2377
Nacho Menendez <nchmnndzlpz@gmail.com>
2024-04-03 13:51:24 +0200

    Actualizar ruta de archivos estáticos en el servidor

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
